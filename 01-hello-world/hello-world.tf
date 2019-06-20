e4aace8979d361f4ff3d7089b0d1aba9058a92c8
Robert Narvaez <bobby.narvy@outlook.com>
2019-06-20 14:54:30 +0000

    add main tf file and provision script

---
+++ 01-hello-world/hello-world.tf
@@ -0,0 +1,31 @@
+variable "do_token" {}
+variable "ssh_pub_key_path" {}
+variable "ssh_prv_key_path" {}
+
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
+resource "digitalocean_ssh_key" "default" {
+    name        = "Default SSH key"
+    public_key  = "${file(var.ssh_pub_key_path)}"
+} 
+
+resource "digitalocean_droplet" "web" {
+    image       = "ubuntu-18-04-x64"
+    name        = "hello-world"
+    region      = "fra1"
+    size        = "s-1vcpu-1gb"
+    ssh_keys    = ["${digitalocean_ssh_key.default.fingerprint}"]
+
+    provisioner "remote-exec" {
+        script  = "provision-script.sh"
+
+        connection {
+            type        = "ssh"
+            host        = "${digitalocean_droplet.web.ipv4_address}"
+            user        = "root"
+            private_key = "${file(var.ssh_prv_key_path)}"
+        }
+    }
+}
