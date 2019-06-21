cb4ed3fff73c3258e0e85a4b4dba98d94d684538
Robert Narvaez <bobby.narvy@outlook.com>
2019-06-21 09:16:22 +0000

    add html and nginx configuration

--- 01-hello-world/hello-world.tf
+++ 01-hello-world/hello-world.tf
@@ -17,7 +17,20 @@ resource "digitalocean_droplet" "web" {
     region      = "fra1"
     size        = "s-1vcpu-1gb"
     ssh_keys    = ["${digitalocean_ssh_key.default.fingerprint}"]
+    depends_on  = ["digitalocean_ssh_key.default"]
 
+    provisioner "file" {
+        source      = "assets"
+        destination = "/home"
+
+        connection {
+            type        = "ssh"
+            host        = "${digitalocean_droplet.web.ipv4_address}"
+            user        = "root"
+            private_key = "${file(var.ssh_prv_key_path)}"
+        }
+    }
+    
     provisioner "remote-exec" {
         script  = "provision-script.sh"
 
--- 01-hello-world/provision-script.sh
+++ 01-hello-world/provision-script.sh
@@ -1,2 +1,13 @@
-sudo apt update
-sudo apt install -y nginx
+# create html directory and move hello world file there
+mkdir /home/html
+mv /home/assets/index.html  /home/html/index.html
+
+# install nginx
+apt update
+apt install -y nginx
+
+# overwrite default nginx configuration
+mv /home/assets/nginx-default.conf /etc/nginx/sites-available/default
+
+# restart nginx
+systemctl restart nginx
