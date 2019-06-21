e09aec255d416ea46e1bc7aac53feb9b2192dd79
Robert Narvaez <bobby.narvy@outlook.com>
2019-06-21 09:46:48 +0000

    reident to 2 spaces

--- 01-hello-world/hello-world.tf
+++ 01-hello-world/hello-world.tf
@@ -3,42 +3,42 @@ variable "ssh_pub_key_path" {}
 variable "ssh_prv_key_path" {}
 
 provider "digitalocean" {
-    token = "${var.do_token}"
+  token = "${var.do_token}"
 }
 
 resource "digitalocean_ssh_key" "default" {
-    name        = "Default SSH key"
-    public_key  = "${file(var.ssh_pub_key_path)}"
+  name        = "Default SSH key"
+  public_key  = "${file(var.ssh_pub_key_path)}"
 } 
 
 resource "digitalocean_droplet" "web" {
-    image       = "ubuntu-18-04-x64"
-    name        = "hello-world"
-    region      = "fra1"
-    size        = "s-1vcpu-1gb"
-    ssh_keys    = ["${digitalocean_ssh_key.default.fingerprint}"]
-    depends_on  = ["digitalocean_ssh_key.default"]
+  image       = "ubuntu-18-04-x64"
+  name        = "hello-world"
+  region      = "fra1"
+  size        = "s-1vcpu-1gb"
+  ssh_keys    = ["${digitalocean_ssh_key.default.fingerprint}"]
+  depends_on  = ["digitalocean_ssh_key.default"]
 
-    provisioner "file" {
-        source      = "assets"
-        destination = "/home"
+  provisioner "file" {
+    source      = "assets"
+    destination = "/home"
 
-        connection {
-            type        = "ssh"
-            host        = "${digitalocean_droplet.web.ipv4_address}"
-            user        = "root"
-            private_key = "${file(var.ssh_prv_key_path)}"
-        }
+    connection {
+      type        = "ssh"
+      host        = "${digitalocean_droplet.web.ipv4_address}"
+      user        = "root"
+      private_key = "${file(var.ssh_prv_key_path)}"
     }
-    
-    provisioner "remote-exec" {
-        script  = "provision-script.sh"
+  }
+  
+  provisioner "remote-exec" {
+    script  = "provision-script.sh"
 
-        connection {
-            type        = "ssh"
-            host        = "${digitalocean_droplet.web.ipv4_address}"
-            user        = "root"
-            private_key = "${file(var.ssh_prv_key_path)}"
-        }
+    connection {
+      type        = "ssh"
+      host        = "${digitalocean_droplet.web.ipv4_address}"
+      user        = "root"
+      private_key = "${file(var.ssh_prv_key_path)}"
     }
+  }
 }
