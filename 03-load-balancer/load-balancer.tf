f0d861c183d1afc5b08334a156508dee08b53c51
Robert Narvaez <narvaez.rm@gmail.com>
2019-06-23 23:14:17 +0000

    add basic setup for single server

---
+++ 03-load-balancer/load-balancer.tf
@@ -0,0 +1,36 @@
+variable "do_token" {}
+variable "ssh_pub_key_path" {}
+variable "ssh_prv_key_path" {}
+
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name        = "Default SSH key"
+  public_key  = "${file(var.ssh_pub_key_path)}"
+} 
+
+resource "digitalocean_droplet" "lb-back" {
+  image       = "ubuntu-18-04-x64"
+  name        = "backend-app"
+  region      = "fra1"
+  size        = "s-1vcpu-1gb"
+  ssh_keys    = ["${digitalocean_ssh_key.default.fingerprint}"]
+  depends_on  = ["digitalocean_ssh_key.default"]  
+}
+
+resource "digitalocean_loadbalancer" "load-balancer" {
+  name        = "terrarium-lb"
+  region      = "fra1"
+
+  forwarding_rule {
+    entry_protocol  = "http"
+    entry_port      = 80
+
+    target_protocol = "http"
+    target_port     = 80
+  }
+
+  droplet_ids = ["${digitalocean_droplet.lb-back.id}"]
+}
